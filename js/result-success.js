0ffe7193a8d2e83cc4dcdce671b91f60bf1404af
Vitaly <vitalykrupin@gmail.com>
2018-11-21 12:45:52 +0500

    eslint corrections

--- js/result-success.js
+++ js/result-success.js
@@ -5,8 +5,8 @@ const template = `
   <section class="result">
     <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
     <h2 class="result__title">Вы настоящий меломан!</h2>
-    <p class="result__total">За 3 минуты и 25 секунд вы набрали 12 баллов (8 быстрых), совершив 3 ошибки</p>
-    <p class="result__text">Вы заняли 2 место из 10. Это лучше чем у 80% игроков</p>
+    <p class="result__total">За 3 минуты и 25 секунд вы набрали 12 баллов (8 быстрых), совершив 3 ошибки</p>
+    <p class="result__text">Вы заняли 2 место из 10. Это лучше чем у 80% игроков</p>
     <button class="result__replay" type="button">Сыграть ещё раз</button>
   </section>
 `;
--- js/welcome.js
+++ js/welcome.js
@@ -8,7 +8,7 @@ const template = `
     <h2 class="welcome__rules-title">Правила игры</h2>
     <p class="welcome__text">Правила просты:</p>
     <ul class="welcome__rules-list">
-      <li>За 5 минут нужно ответить на все вопросы.</li>
+      <li>За 5 минут нужно ответить на все вопросы.</li>
       <li>Можно допустить 3 ошибки.</li>
     </ul>
     <p class="welcome__text">Удачи!</p>
