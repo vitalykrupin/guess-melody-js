95d9e3b0838a901dd14fe308a3aeac1651f60cd1
Vitaly <vitalykrupin@gmail.com>
2018-12-08 11:18:13 +0500

    get results

---
+++ js/data/show-results.js
@@ -0,0 +1,19 @@
+import string from './string-data';
+
+export const showResults = (statistics, gameResult) => {
+  if (gameResult.time <= 0) {
+    return `${string.result.timeLose}`;
+  }
+  if (gameResult.lives <= 0) {
+    return `${string.result.loseStat}`;
+  }
+  const userPoints = gameResult.UserPoint;
+  const points = statistics.map((item) => item.points);
+  points.push(userPoints);
+  points.sort((a, b) => b - a);
+  const players = points.length;
+  const place = points.indexOf(userPoints) + 1;
+  const rate = (players - place) / players * 100;
+
+  return `Вы заняли ${place} место из ${players} игроков. Это лучше, чем у ${rate}% игроков`;
+};
