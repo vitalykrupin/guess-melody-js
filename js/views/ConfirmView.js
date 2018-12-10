441d9f9bb109a0cd0b86a387ca8d32e2acff02b7
Vitaly <vitalykrupin@gmail.com>
2018-12-10 18:24:14 +0500

    confirm & error have views now

---
+++ js/views/ConfirmView.js
@@ -0,0 +1,56 @@
+import AbstractView from './AbstractView';
+import content from '../data/game-content';
+
+export default class ConfirmView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `
+      <section class="modal">
+        <button class="modal__close" type="button"><span class="visually-hidden">${content.buttons.modalClose}</span></button>
+        <h2 class="modal__title">${content.modal.title}</h2>
+        <p class="modal__text">${content.modal.confirm}</p>
+        <div class="modal__buttons">
+          <button class="modal__button button">${content.buttons.modalConfirm}</button>
+          <button class="modal__button button">${content.buttons.modalReject}</button>
+        </div>
+      </section>
+    `;
+  }
+
+  showModal() {
+    document.body.appendChild(this.element);
+  }
+
+  closeModal() {
+    document.body.removeChild(document.body.lastElementChild);
+  }
+
+  confirmButtonClickHandler() {}
+
+  closeModalClickHandler() {}
+
+  bind() {
+    const modalButtons = Array.from(this.element.querySelector(`.modal__buttons`).querySelectorAll(`button`));
+    const confirmButton = modalButtons.find((el) => el.textContent === `Ок`);
+    const cancelButton = modalButtons.find((el) => el.textContent === `Отмена`);
+    const closeButton = this.element.querySelector(`.modal__close`);
+
+    confirmButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      this.confirmButtonClickHandler();
+    });
+
+    cancelButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      this.closeModalClickHandler();
+    });
+
+    closeButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      this.closeModalClickHandler();
+    });
+  }
+}
