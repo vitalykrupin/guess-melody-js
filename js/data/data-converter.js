ccec110086662e05af5fd6dac8dac3580e1b77e9
Vitaly <vitalykrupin@gmail.com>
2018-12-10 18:19:47 +0500

    data converts to needed format

---
+++ js/data/data-converter.js
@@ -0,0 +1,39 @@
+const convertArtistAnswers = (answers) => {
+  let screenAnswers = {};
+  answers.forEach((item, i) => {
+    screenAnswers[`artist-${i}`] = {
+      song: {
+        name: item.title,
+        image: item.image.url
+      },
+      correct: item.isCorrect
+    };
+  });
+  return screenAnswers;
+};
+
+const convertGenreAnswers = (answers, genre) => {
+  let screenAnswers = {};
+  answers.forEach((item, i) => {
+    screenAnswers[`answer-${i}`] = {
+      song: {
+        src: item.src
+      },
+      correct: item.genre === genre
+    };
+  });
+  return screenAnswers;
+};
+
+export default (data) => {
+  for (const question of data) {
+    question.type = question.type;
+    question.title = question.question;
+    if (question.type === `artist`) {
+      question.answers = convertArtistAnswers(question.answers);
+    } else if (question.type === `genre`) {
+      question.answers = convertGenreAnswers(question.answers, question.genre);
+    }
+  }
+  return data;
+};
