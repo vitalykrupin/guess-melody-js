63496b4c3d985180cb3df8ccfff688b1d664c7e9
Vitaly <vitalykrupin@gmail.com>
2018-12-11 08:51:06 +0500

    header removed to view & some minor

--- js/screens/player.js
+++ js/screens/player.js
@@ -2,7 +2,7 @@ export const playerGenre = (src) => `
   <div class="track">
     <button class="track__button track__button--play" type="button"></button>
     <div class="track__status">
-      <audio src="${src}"></audio>
+      <audio src="${src}" preload="auto"></audio>
     </div>
   </div>
 `;
@@ -10,6 +10,6 @@ export const playerGenre = (src) => `
 export const playerArtist = (src) => `
   <div class="game__track">
     <button class="track__button track__button--play" type="button"></button>
-    <audio src="${src}"></audio>
+    <audio src="${src}" preload="auto" autoplay></audio>
   </div>
 `;
