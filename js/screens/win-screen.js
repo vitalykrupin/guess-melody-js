769c752c02d3b679509a84939cbdf17e5ee302c7
Vitaly <vitalykrupin@gmail.com>
2018-12-11 08:48:05 +0500

    refactor result screens

--- js/screens/win-screen.js
+++ js/screens/win-screen.js
@@ -1,13 +1,13 @@
 import {showScreen} from '../utils';
 import welcomeScreen from './welcome-screen';
-import {initialState} from '../data/game-data';
+import {InitialState} from '../data/game-data';
 import WinView from '../views/WinView';
 
 export default (state) => {
   const screen = new WinView(state);
 
   screen.replayButtonClickHandler = () => {
-    showScreen(welcomeScreen(initialState));
+    showScreen(welcomeScreen(InitialState));
   };
 
   return screen.element;
